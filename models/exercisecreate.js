6eee81006152e8163ed89f8a2f309fc0bbca5c97
Michael Hoyt <m.mikehoyt@outlook.com>
2020-06-19 19:37:10 -0400

    Forgot to beautify code

--- models/exercisecreate.js
+++ models/exercisecreate.js
@@ -1,67 +1,70 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 const exerciseSchema = new Schema({
-  day: {
-    type: Date,
-   default: Date.now
-   },
+    day: {
+      type: Date,
+      default: Date.now
+    },
 
-  exercises: [
-    //exercises array needed to populate mongoose and create database
-{
+    exercises: [
+      //exercises array needed to populate mongoose and create database
+      {
 
-//defining schema
-  type: {
-    type: String,
-    trim: true,
-    required: "Enter an exercise type"
-    },
-    name: {
-    type: String,
-    trim: true,
-    required: "Enter an exercise name"
-    },
-    duration: {
-    type: Number,
-    required: "Enter an exercise duration in minutes"
-    },
-    weight: {
-     
-    type: Number
-    },
-    reps: {
-    type: Number
-    },
-    sets: {
-    type: Number
-    },
-    distance: {
-    type: Number
-    }
-  }    
-] 
+        //defining schema
+        type: {
+          type: String,
+          trim: true,
+          required: "Enter an exercise type"
+        },
+        name: {
+          type: String,
+          trim: true,
+          required: "Enter an exercise name"
+        },
+        duration: {
+          type: Number,
+          required: "Enter an exercise duration in minutes"
+        },
+        weight: {
+
+          type: Number
+        },
+        reps: {
+          type: Number
+        },
+        sets: {
+          type: Number
+        },
+        distance: {
+          type: Number
+        }
+      }
+    ]
   },
   //needed for virtual field (total duration) to be displayed
   {
     toObject: {
-    virtuals: true
+      virtuals: true
     },
-    
-      toJSON: {
-      virtuals: true 
-      
-      }
-      
-    }    
-);
-  
-    
-    //for declaring virtual attribute on schema client
-    
-    exerciseSchema.virtual("totalDuration").get(function() { 
-      return this.exercises.reduce((total, exercise) => {        return total + exercise.duration;      }, 0);    });
 
-   
+    toJSON: {
+      virtuals: true
+
+    }
+
+  }
+);
+
+
+//for declaring virtual attribute on schema client
+
+exerciseSchema.virtual("totalDuration").get(function () {
+  return this.exercises.reduce((total, exercise) => {
+    return total + exercise.duration;
+  }, 0);
+});
+
+
 //needed for creating database I think
 const Exercise = mongoose.model('Exercise', exerciseSchema)
 
