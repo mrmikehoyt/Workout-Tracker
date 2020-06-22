6eb9ac66b7e25a2cc44cb74b3e88825bcc4461a7
Michael Hoyt <m.mikehoyt@outlook.com>
2020-06-21 23:19:16 -0400

    updated code to seed, need to troubleshoot because duplicates will be created each time code run

--- models/exercisecreate.js
+++ models/exercisecreate.js
@@ -5,7 +5,34 @@ const exerciseSchema = new Schema({
       type: Date,
       default: Date.now
     },
+    type: {
+      type: String,
+      trim: true,
+      required: "Enter an exercise type"
+    },
+    name: {
+      type: String,
+      trim: true,
+      required: "Enter an exercise name"
+    },
+    duration: {
+      type: Number,
+      required: "Enter an exercise duration in minutes"
+    },
+    weight: {
 
+      type: Number
+    },
+    reps: {
+      type: Number
+    },
+    sets: {
+      type: Number
+    },
+    distance: {
+      type: Number
+    },
+  
     exercises: [
       //exercises array needed to populate mongoose and create database
       {
@@ -39,6 +66,7 @@ const exerciseSchema = new Schema({
           type: Number
         }
       }
+      
     ]
   },
   //needed for virtual field (total duration) to be displayed
@@ -65,7 +93,27 @@ exerciseSchema.virtual("totalDuration").get(function () {
 });
 
 
+
 //needed for creating database I think
 const Exercise = mongoose.model('Exercise', exerciseSchema)
-
+//const User = mongoose.model("User", UserSchema);
+//for seeding duplicates are created if run multiple times. need to troubleshoot
+var exercises = [{
+  'type': 'resistance',
+  'name': 'dubmess',
+  'duration': 50,
+  'weight': 564,
+  'reps':6,
+  'sets':4,
+  'distance':6
+  }]
+  
+  Exercise.insertMany(exercises, {ordered:false}, function(err, res){
+    exercises.forEach(function(item){
+        //check exist in res using memberNo
+       
+  
+    })
+  });
 module.exports = Exercise;
+
