78c9b7005d4c9c221e65a4740046ec1e76c3c0ee
Michael Hoyt <m.mikehoyt@outlook.com>
2020-06-22 21:31:37 -0400

    created file for apiget routes

---
+++ routes/workoutapigetroutes.js
@@ -0,0 +1,42 @@
+const router = require('express').Router();
+const Workout = require('../models/workoutschema.js');
+router.get('/api/workouts', (req, res) => {
+
+    Workout.find()
+  
+      .then(dbWorkout => {
+  
+        res.json(dbWorkout);
+  
+      })
+  
+      .catch(err => {
+  
+        res.json(err);
+  
+      });
+  
+  });
+  
+  router.get('/api/workouts/range', (req, res) => {
+  
+    Exercise.find({})
+  
+      .limit(7)
+  
+      .then(dbWorkout => {
+  
+        console.log(dbWorkout);
+  
+        res.json(dbWorkout);
+  
+      })
+  
+      .catch(err => {
+  
+        res.json(err);
+  
+      });
+  
+  });
+  
