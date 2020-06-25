b62cd7325f8e6f15b045cbc190feeb3cc05a3879
Michael Hoyt <m.mikehoyt@outlook.com>
2020-06-24 20:33:59 -0400

    updated comments

--- models/index.js
+++
@@ -1,5 +0,0 @@
-// Exporting Workout(Object) containing all of our models
-
-module.exports = {
-    Workout: require("./workoutschema"),
-    };
--- routes/workouthtmlgetroutes.js
+++ routes/workouthtmlgetroutes.js
@@ -1,19 +1,19 @@
 const router = require('express').Router();
 const path = require('path');
-/* route for exercise.html */
 
+//route for exercise.html
 router.get('/exercise', (req, res) => {
 
     res.sendFile(path.join(__dirname, '../public/exercise.html'));
   
   });
   
-  /* route for stats.html */
+// route for stats.html 
   
   router.get('/stats', (req, res) => {
   
     res.sendFile(path.join(__dirname, '../public/stats.html'));
-    //const Exercise = mongoose.model('Exercise', exerciseSchema)
+    
     
   });
   module.exports = router
